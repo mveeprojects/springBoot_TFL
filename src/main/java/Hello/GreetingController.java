fa93e82d81bf7b62311cea0108cc7e7f0bf8c57a
Mark Vallis <1034528@my.brunel.ac.uk>
2017-09-04 22:09:27 +0100

    added endpoint /callTFL to get status of all lines as-is (not manipulated/mapped to POJO)

--- src/main/java/Hello/GreetingController.java
+++ src/main/java/Hello/GreetingController.java
@@ -5,6 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
@@ -16,5 +22,25 @@ public class GreetingController {
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @RequestMapping("/callTFL")
+    public String callTFLAPI(){
+        try {
+            URL endpoint = new URL("https://api.tfl.gov.uk/line/mode/tube/status");
+            HttpURLConnection conn = (HttpURLConnection) endpoint.openConnection();
+            conn.setRequestMethod("GET");
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String responseJsonString = br.readLine();
+            br.close();
+            return responseJsonString;
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "";
     }
 }
