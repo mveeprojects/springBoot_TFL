e395fa44b454b1af253e7a4289a0552baf1689e1
Mark Vallis <1034528@my.brunel.ac.uk>
2017-09-09 00:15:37 +0100

    outputting line name and status now

--- src/main/java/TubeLineStatus/Controllers/Dashboard.java
+++ src/main/java/TubeLineStatus/Controllers/Dashboard.java
@@ -1,5 +1,6 @@
 package TubeLineStatus.Controllers;
 
+import TubeLineStatus.POJOs.TFLResponsePOJOs.LineStatus;
 import TubeLineStatus.POJOs.TFLResponsePOJOs.TFLResponse;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -8,37 +9,42 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class Dashboard {
 
     private ObjectMapper objectMapper;
+    private List<LineStatus> lineStatusList;
 
     public Dashboard() {
         objectMapper = new ObjectMapper();
+        lineStatusList = new ArrayList<>();
     }
 
     @RequestMapping("/")
-    public List<TFLResponse> appStatus() throws IOException {
-        return callTFLAPI();
+    public List<LineStatus> appStatus() throws IOException {
+        callTFLAPI();
+        return lineStatusList;
     }
 
-    private List<TFLResponse> callTFLAPI() {
+    private void callTFLAPI() {
         try {
             URL url = new URL("https://api.tfl.gov.uk/line/mode/tube/status");
             List<TFLResponse> tflResponse = objectMapper.readValue(url, new TypeReference<List<TFLResponse>>() {
             });
 
             for (TFLResponse t : tflResponse) {
-                System.out.println(t.getName());
-                System.out.println(t.getStatus()+"\n");
-            }
-            return tflResponse;
 
+                System.out.println(t.getName());
+                System.out.println(t.getStatus() + "\n");
+
+                LineStatus lineStatus = new LineStatus(t.getName(), t.getStatus());
+                lineStatusList.add(lineStatus);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return null;
     }
 }
---
+++ src/main/java/TubeLineStatus/POJOs/TFLResponsePOJOs/LineStatus.java
@@ -0,0 +1,19 @@
+package TubeLineStatus.POJOs.TFLResponsePOJOs;
+
+public class LineStatus {
+    private String lineName;
+    private String lineStatus;
+
+    public LineStatus(String lineName, String lineStatus) {
+        this.lineName = lineName;
+        this.lineStatus = lineStatus;
+    }
+
+    public String getLineName() {
+        return lineName;
+    }
+
+    public String getLineStatus() {
+        return lineStatus;
+    }
+}
