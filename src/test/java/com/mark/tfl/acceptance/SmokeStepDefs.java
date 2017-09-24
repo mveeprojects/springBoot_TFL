c37cad842856bc4a0150e5f2bd8aac693f995699
Mark Vallis <1034528@my.brunel.ac.uk>
2017-09-24 18:19:04 +0100

    added and implemted step defs to check for empty response body and status code seperately

--- src/test/java/com/mark/tfl/acceptance/SmokeStepDefs.java
+++ src/test/java/com/mark/tfl/acceptance/SmokeStepDefs.java
@@ -1,6 +1,7 @@
 package com.mark.tfl.acceptance;
 
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -9,14 +10,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.assertThat;
 
 public class SmokeStepDefs {
 
     private static Logger LOGGER = LoggerFactory.getLogger(SmokeStepDefs.class);
+    private int statusCode = 0;
+    CloseableHttpResponse response;
 
-    @Given("^I call the '(.+)' endpoint locally I should receive status code '(\\d+)'$")
-    public void iCallAnEndpointLocally(String endpoint, int expectedStatus) throws Throwable {
+    @Given("^I call the '(.+)' endpoint locally$")
+    public void iCallAnEndpointLocally(String endpoint) throws Throwable {
         LOGGER.info("Calling the {} endpoint locally", "issues");
 
         CloseableHttpClient httpClient = HttpClients.createDefault();
@@ -24,10 +28,14 @@ public class SmokeStepDefs {
         String url = "http://localhost:8080" + endpoint;
 
         HttpGet request = new HttpGet(url);
-        CloseableHttpResponse response = httpClient.execute(request);
+        response = httpClient.execute(request);
+        String s = response.toString();
+        assertThat(s, not(""));
+    }
 
-        int actualStatus = response.getStatusLine().getStatusCode();
-
-        assertThat(actualStatus ,is(expectedStatus));
+    @Then("^I should receive a status code of '(\\d+)'$")
+    public void iShouldReceiveAStatusCodeOf(int expectedStatus) throws Throwable {
+        statusCode = response.getStatusLine().getStatusCode();
+        assertThat(statusCode, is(expectedStatus));
     }
 }
--- src/test/resources/com.mark.tfl.acceptance/SmokeTests.feature
+++ src/test/resources/com.mark.tfl.acceptance/SmokeTests.feature
@@ -1,10 +1,17 @@
 Feature: Run smoke tests against app endpoints
 
   Scenario: Calling the main entrypoint of the app, the app should return a http status code of 200
-    When I call the '/' endpoint locally I should receive status code '200'
+    Given I call the '/' endpoint locally
+    Then I should receive a status code of '200'
 
   Scenario: Calling the issues endpoint, the app should return a http status code of 200
-    When I call the '/issues' endpoint locally I should receive status code '200'
+    When I call the '/issues' endpoint locally
+    Then I should receive a status code of '200'
 
   Scenario: Calling the checkline endpoint, the app should return a http status code of 200
-    When I call the '/checkline?line=metropolitan' endpoint locally I should receive status code '200'
+    When I call the '/checkline?line=metropolitan' endpoint locally
+    Then I should receive a status code of '200'
+
+  Scenario: Calling an unknown endpoint, the app should return a http status code of 404
+    When I call the '/somejunk' endpoint locally
+    Then I should receive a status code of '404'
