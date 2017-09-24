c1e91d33c835220c9d65a05d1e9b5716dc5e7822
Mark Vallis <1034528@my.brunel.ac.uk>
2017-09-24 15:55:11 +0100

    refactoring java logic into a seperate service class

--- src/main/java/TubeLineStatus/Controllers/Dashboard.java
+++ src/main/java/TubeLineStatus/Controllers/Dashboard.java
@@ -1,76 +1,28 @@
 package TubeLineStatus.Controllers;
 
 import TubeLineStatus.POJOs.LineStatus;
-import TubeLineStatus.POJOs.TFLResponse;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import TubeLineStatus.Services.TFLStatusService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 @RestController
 public class Dashboard {
-
-    private ObjectMapper objectMapper;
-    private List<LineStatus> lineStatusList;
-    private List<LineStatus> lineIssuesList;
-    List<TFLResponse> tflResponse;
+    private TFLStatusService tflStatusService;
 
     public Dashboard() {
-        objectMapper = new ObjectMapper();
-        lineStatusList = new ArrayList<>();
-        lineIssuesList = new ArrayList<>();
+        tflStatusService = new TFLStatusService();
     }
 
     @RequestMapping("/")
-    public List<LineStatus> appStatus() throws IOException {
-        if (lineStatusList.isEmpty()) {
-            callTFLAPI();
-        }
-        allLineStatuses();
-        return lineStatusList;
+    public List<LineStatus> allLinesStatuses() throws IOException {
+        return tflStatusService.getAllLineStatuses();
     }
 
     @RequestMapping("/issues")
-    public List<LineStatus> affectedLines() {
-        if (lineIssuesList.isEmpty()) {
-            detectLineIssues();
-        }
-        return lineIssuesList;
-    }
-
-    private void allLineStatuses() {
-        for (TFLResponse t : tflResponse) {
-            LineStatus lineStatus = new LineStatus(t.getName(), t.getStatus());
-            lineStatusList.add(lineStatus);
-        }
-    }
-
-    private void callTFLAPI() {
-        try {
-            URL url = new URL("https://api.tfl.gov.uk/line/mode/tube/status");
-            tflResponse = objectMapper.readValue(url, new TypeReference<List<TFLResponse>>() {
-            });
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void detectLineIssues() {
-        if (lineStatusList.isEmpty()) {
-            callTFLAPI();
-            allLineStatuses();
-        }
-        for (LineStatus lineStatus : lineStatusList) {
-            String status = lineStatus.getLineStatus();
-            if (!Objects.equals(status, "Good Service")) {
-                lineIssuesList.add(lineStatus);
-            }
-        }
+    public List<LineStatus> linesWithIssues() {
+        return tflStatusService.getLinesWithIssues();
     }
 }
--- src/main/java/TubeLineStatus/Services/TFLStatusService.java
+++ src/main/java/TubeLineStatus/Services/TFLStatusService.java
@@ -1,7 +1,79 @@
 package TubeLineStatus.Services;
 
+import TubeLineStatus.POJOs.LineStatus;
+import TubeLineStatus.POJOs.TFLResponse;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TFLStatusService {
 
+    private ObjectMapper objectMapper;
+    private List<LineStatus> allLineStatuses;
+    private List<LineStatus> linesWithIssues;
+    private List<TFLResponse> tflResp;
 
+    public TFLStatusService() {
+        objectMapper = new ObjectMapper();
+        linesWithIssues = new ArrayList<>();
+        tflResp = new ArrayList<>();
+        allLineStatuses = new ArrayList<>();
+    }
+
+    public List<LineStatus> getAllLineStatuses() {
+        runAllStatusCheck();
+        return allLineStatuses;
+    }
+
+    public List<LineStatus> getLinesWithIssues() {
+        return linesWithIssues;
+    }
+
+    private void runAllStatusCheck() {
+        if (tflResp.isEmpty() || allLineStatuses.isEmpty()) {
+            callTFLAPI();
+            getLineStatuses();
+        }
+    }
+
+    private void callTFLAPI() {
+        try {
+            URL url = new URL("https://api.tfl.gov.uk/line/mode/tube/status");
+            tflResp = objectMapper.readValue(url, new TypeReference<List<TFLResponse>>() {
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void getLineStatuses() {
+        for (TFLResponse tflResponse : tflResp) {
+            LineStatus lineStatus = new LineStatus(tflResponse.getName(), tflResponse.getStatus());
+            allLineStatuses.add(lineStatus);
+        }
+    }
+
+
+//    private List<LineStatus> detectLineIssues() {
+//
+//        if (allLineStatuses.isEmpty()) {
+//            getAllLineStatuses = allLineStatuses();
+//        }
+//
+//        if (linesWithIssues.isEmpty()) {
+//            getAllLineStatuses();
+//        }
+//        for (LineStatus lineStatus : linesWithIssues) {
+//            String status = lineStatus.getLineStatus();
+//            if (!Objects.equals(status, "Good Service")) {
+//                linesWithIssues.add(lineStatus);
+//            }
+//        }
+//        return linesWithIssues;
+//    }
 
 }
