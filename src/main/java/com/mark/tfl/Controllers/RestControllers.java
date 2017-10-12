079a2ca8273ddb1d2c11fc4d4dec58352d5589a7
Mark Vallis <1034528@my.brunel.ac.uk>
2017-10-12 18:44:09 +0100

    refactored RestControllers class

--- src/main/java/com/mark/tfl/Controllers/RestControllers.java
+++ src/main/java/com/mark/tfl/Controllers/RestControllers.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.util.List;
 
 import static org.springframework.util.StringUtils.isEmpty;
+
 @RestController
 public class RestControllers {
 
@@ -22,7 +23,7 @@ public class RestControllers {
     @Autowired
     private static TFLStatusService tflStatusService;
 
-    public static TFLStatusService getTflStatusService() {
+    static TFLStatusService getTflStatusService() {
         return tflStatusService;
     }
 
