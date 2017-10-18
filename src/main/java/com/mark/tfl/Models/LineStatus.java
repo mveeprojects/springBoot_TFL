a68c669e8ceb605619eae31cbc4cd0d98d90054d
Mark Vallis <1034528@my.brunel.ac.uk>
2017-10-18 19:27:50 +0100

    added icons to indicate day/night lines

--- src/main/java/com/mark/tfl/Models/LineStatus.java
+++ src/main/java/com/mark/tfl/Models/LineStatus.java
@@ -3,10 +3,12 @@ package com.mark.tfl.Models;
 public class LineStatus {
     private String lineName;
     private String lineStatus;
+    private boolean nightTube;
 
     public LineStatus(String lineName, String lineStatus) {
         this.lineName = lineName;
         this.lineStatus = lineStatus;
+        this.nightTube = isNightTube(lineName);
     }
 
     public String getLineName() {
@@ -17,11 +19,29 @@ public class LineStatus {
         return lineStatus;
     }
 
+    public boolean isNightTube() {
+        return nightTube;
+    }
+
+    private boolean isNightTube(String lineName) {
+        switch (lineName) {
+            case "Victoria":
+            case "Central":
+            case "Jubilee":
+            case "Northern":
+            case "Piccadilly":
+                return true;
+            default:
+                return false;
+        }
+    }
+
     @Override
     public String toString() {
         return "LineStatus{" +
                 "lineName='" + lineName + '\'' +
                 ", lineStatus='" + lineStatus + '\'' +
+                ", nightTube=" + nightTube +
                 '}';
     }
 }
