9231fe54d8ab6705c3a623441f7a2cfc8d0ebdac
Mark Vallis <1034528@my.brunel.ac.uk>
2017-10-29 10:14:20 +0000

    fixed unit tests, now using mockito and wiring in required beans

--- src/test/junit/com/mark/tfl/ServiceTests.java
+++ src/test/junit/com/mark/tfl/ServiceTests.java
@@ -1,38 +1,56 @@
 package com.mark.tfl;
 
+import com.mark.tfl.Controllers.TFLRepository;
+import com.mark.tfl.Models.LineStatus;
+import com.mark.tfl.Services.TFLStatusService;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import static com.mark.tfl.Services.TimeService.getCurrentTime;
+import static java.util.Collections.emptyList;
+
+@RunWith(MockitoJUnitRunner.class)
 public class ServiceTests {
 
-//    @Test
-//    public void refreshLineStatusesTest(){
-//        TFLStatusService tflStatusService = new TFLStatusService();
-//        List<LineStatus> oldStatuses = tflStatusService.getLineStatuses();
-//
-//        tflStatusService.scheduleAPICall();
-//        List<LineStatus> newStatuses = tflStatusService.getLineStatuses();
-//
-//        Assert.assertEquals(emptyList(), oldStatuses);
-//        Assert.assertNotEquals(emptyList(), newStatuses);
-//    }
-//
-//    @Test
-//    public void refreshLineIssuesTest(){
-//        TFLStatusService tflStatusService = new TFLStatusService();
-//        List<LineStatus> oldIssues = tflStatusService.getLineIssues();
-//
-//        tflStatusService.scheduleAPICall();
-//        List<LineStatus> newIssues = tflStatusService.getLineIssues();
-//
-//        Assert.assertEquals(emptyList(), oldIssues);
-//        Assert.assertNotEquals(emptyList(), newIssues);
-//    }
-//
-//    @Test
-//    public void checkTimeTest(){
-//        String actual = getCurrentTime();
-//
-//        LocalTime time = LocalTime.now();
-//        String expected = time.format(DateTimeFormatter.ofPattern("h:mm a"));
-//
-//        Assert.assertEquals(expected, actual);
-//    }
+    @Mock
+    private TFLRepository tflRepository;
+
+    @InjectMocks
+    private TFLStatusService tflStatusService;
+
+    @Test
+    public void refreshLineStatusesTest(){
+        List<LineStatus> oldStatuses = tflStatusService.getLineStatuses();
+        tflStatusService.scheduleAPICall();
+        List<LineStatus> newStatuses = tflStatusService.getLineStatuses();
+
+        Assert.assertNotEquals(oldStatuses, newStatuses);
+    }
+
+    @Test
+    public void refreshLineIssuesTest(){
+        List<LineStatus> oldIssues = tflStatusService.getLineIssues();
+        tflStatusService.scheduleAPICall();
+        List<LineStatus> newIssues = tflStatusService.getLineIssues();
+
+        Assert.assertEquals(emptyList(), oldIssues);
+        Assert.assertNotEquals(emptyList(), newIssues);
+    }
+
+    @Test
+    public void checkTimeTest(){
+        String actual = getCurrentTime();
+        LocalTime time = LocalTime.now();
+        String expected = time.format(DateTimeFormatter.ofPattern("h:mm a"));
+
+        Assert.assertEquals(expected, actual);
+    }
 }
