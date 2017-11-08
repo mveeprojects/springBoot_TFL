ec009ba02c9377ed78d142ba4591b4c2a50ffa90
Mark Vallis <1034528@my.brunel.ac.uk>
2017-11-08 20:20:55 +0000

    tested chart with some values as 0, working as expected (not showing these values)

--- src/main/java/com/mark/tfl/Controllers/TFLController.java
+++ src/main/java/com/mark/tfl/Controllers/TFLController.java
@@ -1,10 +1,7 @@
 package com.mark.tfl.Controllers;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mark.tfl.Models.TFLLineHistoryObject;
 import com.mark.tfl.Services.TFLStatusService;
-import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,11 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import static com.mark.tfl.Utils.MathUtils.getPercentage;
 
@@ -71,96 +64,20 @@ public class TFLController {
 
     @RequestMapping("/chart")
     public String exampleChartModel (Model model){
-        List<Map<String, Integer>> mapsList = new ArrayList<>();
-
-        Map<String, Integer> map1 = new HashMap<>();
-        map1.put("UK", 12);
-        Map<String, Integer> map2 = new HashMap<>();
-        map2.put("Germany", 32);
-        Map<String, Integer> map3 = new HashMap<>();
-        map3.put("USA", 16);
-
-        mapsList.add(map1);
-        mapsList.add(map2);
-        mapsList.add(map3);
-
-        ObjectMapper mapper = new ObjectMapper();
-
-        try {
-            mapper.writeValueAsString(mapsList);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-//        model.addAttribute("mapsList", mapsList);
         model.addAttribute("mapsList", someMethod());
-//        model.addAttribute("mapsList", someOtherMethod());
-
         return "charttest";
     }
 
     public Object[][] someMethod(){
-        Object[][] result = new Object[3][2];
+        Object[][] result = new Object[4][2];
         result[0][0] = "Country";
         result[0][1] = "Number";
         result[1][0] = "IE";
         result[1][1] = 7;
         result[2][0] = "UK";
         result[2][1] = 12;
+        result[3][0] = "Germany";
+        result[3][1] = 0;
         return result;
     }
-
-    public List<Map<String, Integer>> someOtherMethod(){
-
-        List<Map<String, Integer>> result = new ArrayList<>();
-
-        Map<String, Integer> map1 = new HashMap<>();
-        map1.put("UK", 12);
-        Map<String, Integer> map2 = new HashMap<>();
-        map2.put("Germany", 32);
-        Map<String, Integer> map3 = new HashMap<>();
-        map3.put("USA", 16);
-
-        return result;
-    }
-
-    @Test
-    public void someTest() throws IOException {
-        List<String> animals = new ArrayList<>();
-        String lion = "Samba";
-        String elephant = "Manny";
-        animals.add(lion);
-        animals.add(elephant);
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.writeValue(System.out, animals);
-    }
-
-    @Test
-    public void someOtherTest() throws IOException {
-
-        List<Map<String, Integer>> mapsList = new ArrayList<>();
-
-        Map<String, Integer> map1 = new HashMap<>();
-        map1.put("UK", 12);
-        Map<String, Integer> map2 = new HashMap<>();
-        map2.put("Germany", 32);
-        Map<String, Integer> map3 = new HashMap<>();
-        map3.put("USA", 16);
-
-        mapsList.add(map1);
-        mapsList.add(map2);
-        mapsList.add(map3);
-
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.writeValueAsString(mapsList);
-    }
-
-
-//    @Test
-//    public void test(){
-//        Map<String, Integer> someData = new HashMap<>();
-//        someData.put("UK", 12);
-//        someData.put("Canada", 7);
-//        someData.put("USA", 32);
-//        System.out.println(someData.toString());
-//    }
 }
