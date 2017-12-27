d302743aab4249ffb6c5c83ecf5eea8c59174648
Mark Vallis <markvallis1987@Gmail.com>
2017-12-27 15:50:08 +0000

    manual search no longer persists results in mongo

--- src/main/java/com/mark/tfl/Services/TFLStatusService.java
+++ src/main/java/com/mark/tfl/Services/TFLStatusService.java
@@ -63,7 +63,6 @@ public class TFLStatusService {
         callTFLEndpoint();
         runAllLineStatusesCheck();
         runLinesWithIssuesCheck();
-        saveToMongo();
     }
 
     private void saveToMongo() {
